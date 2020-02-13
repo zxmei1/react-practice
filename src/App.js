1074c91ade60da202a3455b9ed6b428538df580e
zxmei1 <mei1@bu.edu>
2020-02-12 23:57:15 -0500

    practice

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    let courses = [
+        new Course('CAS', 'PY', '251', 'A1', 'Sushkov', 'CAS PY 251'),
+        new Course('CAS', 'CS', '111', 'C1', 'Sullivan', 'CAS CS 111'),
+        new Course('CAS', 'CS', '350', 'A1', 'Sarkar', 'CAS CS 350'),
+    ];
+    return (
+        <div className="App">
+          <table>
+            <thead>
+              <tr>
+                <th>College</th>
+                <th>Department</th>
+                <th>Number</th>
+                <th>Section</th>
+                <th>Professor</th>
+              </tr>
+            </thead>
+            <tbody>
+            {
+              courses.map(course => <tr id={course.key}><th>{course.college}</th><th>{course.department}</th><th>{course.number}</th>
+                <th>{course.section}</th><th>{course.professor}</th></tr>)
+            }
+            </tbody>
+          </table>
+        </div>
+    );
+  }
+
 }
+
+class Course {
+  constructor(college, dpt, num, section, prof, key) {
+    this.college = college;
+    this.department = dpt;
+    this.number = num;
+    this.section = section;
+    this.professor = prof;
+    this.key = key;
+  }
+}
+
 
 export default App;
